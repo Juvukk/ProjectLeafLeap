c669fbbe943580e7505d2b8dfa06f7834f996932
Christer Bjoerk <Chriz.gamedev@gmail.com>
2022-09-19 09:18:10 +1000

    Object pooling works again

--- Assets/Scripts/Object Pooling/ObjectPoolingReturnGameObject.cs
+++ Assets/Scripts/Object Pooling/ObjectPoolingReturnGameObject.cs
@@ -3,24 +3,12 @@ using UnityEngine;
 public class ObjectPoolingReturnGameObject : MonoBehaviour
 {
     private ObjectPooling objectPool;
-    [SerializeField] private float deactivationTimer;
-    private float timer;
 
     private void Start()
     {
         objectPool = FindObjectOfType<ObjectPooling>();
     }
 
-    private void Update()
-    {
-        timer += Time.deltaTime;
-
-        if (timer <= deactivationTimer) return;
-
-        timer = 0;
-
-        objectPool.ReturnGameObject(this.gameObject);
-    }
 
     private void OnTriggerEnter(Collider other)
     {
