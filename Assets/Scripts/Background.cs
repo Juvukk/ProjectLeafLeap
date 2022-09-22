93e3c00400e5c8d0dea12d0e1e46137a219ca396
Andrew Hickin <andrew.hickin0@gmail.com>
2022-09-22 18:34:47 +1000

    minor bug fixes and lighting adjustments

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -4,22 +4,74 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
-    [SerializeField] private Transform restartPos; private Rigidbody rb;
+    [SerializeField] private List<GameObject> backgroundObjects = new List<GameObject>();
+    [SerializeField] private Transform spawnPoint;
+
+    [Tooltip("When a background object reaches this point, spawn a new background object")]
+    [SerializeField] private float zThreshold = -25f;
+
+    private ObjectPooling objectPooling;
+    private List<GameObject> spawnedBkgObjects = new List<GameObject>();
+
+    private GameObject bkg;
+    private int lastRandomizedBkg;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        objectPooling = FindObjectOfType<ObjectPooling>();
     }
 
-    private void OnTriggerEnter(Collider other)
+    // Update is called once per frame
+    private void Update()
     {
-        Debug.Log("collision");
-        if (other.CompareTag("Destroyer"))
-        {
-            Debug.Log("collision with destroyer-reset");
+        SpawnBkgObjects();
+    }
 
-            transform.position = restartPos.position;
-            rb.velocity = Vector3.zero;
+    /// <summary>
+    /// Spawn randomized background objects
+    /// </summary>
+    private void SpawnBkgObjects()
+    {
+        var randomBkg = Random.Range(0, backgroundObjects.Count);
+
+        if (bkg == null)
+        {
+            // Spawn a randomized background object
+            bkg = objectPooling.GetObject(backgroundObjects[randomBkg]);
+
+            // Set spawned background object to parent
+            // for organization purposes
+            bkg.transform.parent = spawnPoint;
+
+            // Set the position of background object to the same as the spawn point
+            bkg.transform.position = spawnPoint.position;
+
+            spawnedBkgObjects.Add(bkg);
+        }
+        else if (bkg != null && randomBkg != lastRandomizedBkg)
+        {
+            lastRandomizedBkg = randomBkg;
+
+            // If an background already exists
+            // and is x floats on the Z-Axis from the previously spawned background object
+            // spawn a new background object
+            if (spawnedBkgObjects[spawnedBkgObjects.Count - 1].transform.position.z >= zThreshold)
+            {
+                var newbkg = objectPooling.GetObject(backgroundObjects[randomBkg]);
+
+                newbkg.transform.parent = spawnPoint;
+
+                // Clear list as we only want the last spawned background object
+                spawnedBkgObjects.Clear();
+                spawnedBkgObjects.Add(newbkg);
+
+                // Set the position of background object to the same as the spawn point
+                newbkg.transform.position = spawnPoint.position;
+            }
+        }
+        else if (bkg != null && randomBkg == lastRandomizedBkg)
+        {
+            randomBkg = Random.Range(0, backgroundObjects.Count);
         }
     }
 }
